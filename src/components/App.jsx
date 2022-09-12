3e39ea59091368ab4158e77ea02358293621aad9
Darya <daryabobur@gmail.com>
2022-09-12 22:15:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FedbackOptions';
+
+
+
 
 class App extends Component {
   state = {
@@ -20,7 +25,6 @@ addRating = () => {
 this.setState(prevState => {
   return {
     good: prevState.good + 1,
-    
   }
 
 })
@@ -28,6 +32,7 @@ this.setState(prevState => {
 
 countTotalFeedback() {
 return Object.values(this.state).reduce((total, rating) => total + rating, 0);
+ 
 }
 
 countPositiveFeedbackPercentage() {
@@ -36,25 +41,30 @@ countPositiveFeedbackPercentage() {
 }
 
 render() {
-
+  const { good, neutral, bad } = this.state;
   return (
-    <>
+
+    
+    <div>
     <h2>Please leave feedback</h2>
 
-      <div>
-      {Object.keys(this.state).map(key => (<button key={key} type="button" onClick={() => this.handleClick(key)} className={this.props.addRating}>{key}</button>
-      ))}
-    </div>
 
-    <h2>Statistics</h2>
-    <ul>
-      <li>Good:{this.state.good}</li>
-      <li>Neutral:{this.state.neutral}</li>
-      <li>Bad:{this.state.bad}</li>
-      <li>Total:{this.countTotalFeedback()}</li>
-      <li>Positive feedback: {this.countPositiveFeedbackPercentage()}%</li>
-    </ul>
-    </>
+      {/* {Object.keys(this.state).map((key) => ( */}
+      <FeedbackOptions key={this.props.state} options={this.handleClick} onLeaveFeedback={this.props.addRating} />
+      {/* // <button key={key} type="button" onClick={() => this.handleClick(key)} className={this.props.addRating}>{key}</button> */}
+      {/* ))} */}
+
+
+    
+
+    <Statistics 
+    good={good} 
+    neutral={neutral} 
+    bad={bad} 
+    total ={this.countTotalFeedback()} 
+    positivePercentage={this.countPositiveFeedbackPercentage()} 
+    />
+    </div>
   )}
 
   
